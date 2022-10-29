b4d37b716e0e6dfdc8b43c4815fceaed5a3120a4
p0wered <powered.ui@gmail.com>
2022-10-29 21:27:00 +0300

    0.0.1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
-      <nav>
-        <div class="logo-box">
-          <h1>IT-STUDIO</h1>
-        </div>
-        <ul>
-          <li><router-link to="">HOME</router-link></li>
-          <li><router-link to="">PROJECTS</router-link></li>
-          <li><router-link to="">PRICING</router-link></li>
-          <li><router-link to="">NEWS</router-link></li>
-          <li><router-link to="">CONTACTS</router-link></li>
-        </ul>
-      </nav>
+<!--      <nav>-->
+<!--        <div class="container">-->
+<!--          <div class="nav-flexbox">-->
+<!--            <div class="logo-box">-->
+<!--              <h1>MIRAGE</h1>-->
+<!--            </div>-->
+<!--            <ul>-->
+<!--              <li><router-link to="">HOME</router-link></li>-->
+<!--              <li><router-link to="">PROJECTS</router-link></li>-->
+<!--              <li><router-link to="">PRICING</router-link></li>-->
+<!--              <li><router-link to="">NEWS</router-link></li>-->
+<!--              <li><router-link to="">CONTACTS</router-link></li>-->
+<!--            </ul>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </nav>-->
+    <vs-row w="12">
+      <vs-col offset="5" w="6">
+        offset = 6
+      </vs-col>
+    </vs-row>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from "@/router";
+import Vue from 'vue'
+import Vuesax from 'vuesax'
+
+import 'vuesax/dist/vuesax.css' //Vuesax styles
+Vue.use(Vuesax, {
+    // options here
+})
 
 createApp(App).use(router).mount('#app')
