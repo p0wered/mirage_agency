4bf0a27743da9544ea8c04725ec3a9f4f5bcbc3a
p0wered <powered.ui@gmail.com>
2022-11-01 20:53:35 +0300

    0.0.4

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,17 +3,39 @@
     <section class="home-section-1">
       <div class="container">
         <div class="home-flexbox-1">
-          <div class="hf1-inner-text">
-            <h3>Creative Agency</h3>
-            <h1>We Deliver The Best Product for You</h1>
-            <p>Design is the creation of a plan or convention for the construction of an object or a system as in architectural blueprints, engineering drawings and business processes</p>
+          <div class="col-lg-5 hf1-text">
+            <h3 class="inner-title-sm">Creative Agency</h3>
+            <h1 class="inner-title-lg">We Deliver The Best Product for You</h1>
+            <p class="inner-desc">Design is the creation of a plan or convention for the construction of an object or a system as in architectural blueprints, engineering drawings and business processes</p>
           </div>
-          <img class="recommended-img" src="../assets/Recommended.png">
+          <div class="hf1-image-box">
+            <img class="recommended-img" src="../assets/Recommended.png">
+          </div>
         </div>
       </div>
     </section>
     <section class="home-section-2">
-
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <h1 data-aos-delay="300" data-aos="fade-in">HUI</h1>
     </section>
   </div>
 </template>
@@ -23,7 +45,6 @@ export default {
   name: "HomePage",
 }
 </script>
-
 <style scoped>
 
 </style>
