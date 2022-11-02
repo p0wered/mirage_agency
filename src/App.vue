cfb8e0d6e433bb059a7db5f5ff523abda0db603e
p0wered <powered.ui@gmail.com>
2022-11-02 22:35:18 +0300

    0.0.4

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
             <div class="logo-box">
               <h1>MIRAGE</h1>
             </div>
-              <ul>
+              <ul class="d-none d-lg-flex">
                 <li><router-link :to="{name:'home'}">HOME</router-link></li>
                 <li><router-link :to="{name:'projects'}">PROJECTS</router-link></li>
                 <li><router-link :to="{name:'pricing'}">PRICING</router-link></li>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,7 +3,7 @@
     <section class="home-section-1">
       <div class="container">
         <div class="home-flexbox-1">
-          <div class="col-lg-5 hf1-text">
+          <div class="col-lg-5">
             <h3 class="inner-title-sm">Creative Agency</h3>
             <h1 class="inner-title-lg">We Deliver The Best Product for You</h1>
             <p class="inner-desc">Design is the creation of a plan or convention for the construction of an object or a system as in architectural blueprints, engineering drawings and business processes</p>
@@ -15,27 +15,14 @@
       </div>
     </section>
     <section class="home-section-2">
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <h1 data-aos-delay="300" data-aos="fade-in">HUI</h1>
+      <div class="container">
+        <div class="home-flexbox-2">
+          <div class="hf2-text">
+            <h3>Our services</h3>
+            <h1>Why People Choose Us</h1>
+          </div>
+        </div>
+      </div>
     </section>
   </div>
 </template>
