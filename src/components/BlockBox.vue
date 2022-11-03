08b656fe2afa70aefefce262bf085ecece5368cc
p0wered <powered.ui@gmail.com>
2022-11-03 21:26:32 +0300

    0.0.5

---
+++ src/components/BlockBox.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>fdfsf</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BlockBox"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -4,9 +4,9 @@
       <div class="container">
         <div class="home-flexbox-1">
           <div class="col-lg-5">
-            <h3 class="inner-title-sm">Creative Agency</h3>
-            <h1 class="inner-title-lg">We Deliver The Best Product for You</h1>
-            <p class="inner-desc">Design is the creation of a plan or convention for the construction of an object or a system as in architectural blueprints, engineering drawings and business processes</p>
+            <h3 class="title-sm-1" style="color: #7b98ff;">Creative Agency</h3>
+            <h1 class="title-lg-1">We Deliver The Best Product for You</h1>
+            <p class="desc-1">Design is the creation of a plan or convention for the construction of an object or a system as in architectural blueprints, engineering drawings and business processes</p>
           </div>
           <div class="hf1-image-box">
             <img class="recommended-img" src="../assets/Recommended.png">
@@ -18,8 +18,8 @@
       <div class="container">
         <div class="home-flexbox-2">
           <div class="hf2-text">
-            <h3>Our services</h3>
-            <h1>Why People Choose Us</h1>
+            <h3 class="title-sm-2">Our services</h3>
+            <h1 class="title-lg-2">Why People Choose Us</h1>
           </div>
         </div>
       </div>
