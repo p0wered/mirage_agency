3cc739d67d9ea5589a860948b40de9dcb02951ff
p0wered <powered.ui@gmail.com>
2022-11-09 21:45:07 +0300

    0.6.3

--- src/pages/ProjectsPage.vue
+++ src/pages/ProjectsPage.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="pr">
-
+  <div class="hui">
+    <Flicking :options="{ align: 'prev', circular: true }" @move-end="onMoveEnd">
+      <div class="panel">1</div>
+      <div class="panel">2</div>
+      <div class="panel">3</div>
+    </Flicking>
   </div>
 </template>
 
