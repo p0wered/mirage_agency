db795109f496d9b27b1e03306e884c4c1f8259be
p0wered <powered.ui@gmail.com>
2022-11-19 18:12:49 +0300

    0.9

--- src/pages/PricingPage.vue
+++ src/pages/PricingPage.vue
@@ -3,35 +3,59 @@
     <section class="pricing-section">
       <div class="container">
         <div class="pricing-flexbox-lg">
-          <h1 class="title-lg-row">Choose your <span style="color: #7b98ff; font-weight: 700">rate</span></h1>
-          <div class="pricing-flexbox-sm">
-            <div class="pricing-item prt-1">
+          <div data-aos="fade" data-aos-duration="600">
+            <h1 class="title-lg-row">Choose your <span style="color: #7b98ff; font-weight: 700">rate</span></h1>
+          </div>
+          <div class="pricing-flexbox-sm" data-aos="fade-up" data-aos-duration="600">
+            <div class="pricing-item">
               <div class="title-box">
                 <h1 class="title-lg-row" style="margin: 0">STANDARD</h1>
               </div>
               <div class="title-desc-box">
-                <ul>
+                <ul class="features-list">
                   <li><i class="bi bi-check-circle-fill"></i><p>Quick start within one day</p></li>
                   <li><i class="bi bi-check-circle-fill"></i><p>No equipment and support costs</p></li>
                   <li><i class="bi bi-check-circle-fill"></i><p>Personal data privacy</p></li>
                   <li><i class="bi bi-check-circle-fill"></i><p>The price depends on the number of customer profiles</p></li>
                 </ul>
+                <p class="num-text">Number of customer profiles</p>
+                <vs-slider :min="5" @change="cambio" color="#7b98ff" step=25 text-fixed=K v-model="value1"/>
+                <div style="text-align: center;">
+                  <h3 style="margin: 1.325rem 0;">{{value2}}$/month</h3>
+                </div>
+                <p class="desc-sm">All payments in Dollars are subject to a 15% commission for acquiring services.
+                  Unfortunately, this is out of our control and there is nothing we can do about it. But you can always
+                  use Rubles or Yuan to pay without commission
+                </p>
               </div>
             </div>
-            <div class="pricing-item prt-2">
+            <div class="pricing-item">
               <div class="title-box">
                 <h1 class="title-lg-row" style="margin: 0">PREMIUM</h1>
               </div>
               <div class="title-desc-box">
-                <ul>
+                <ul class="features-list">
                   <li><i class="bi bi-check-circle-fill"></i><p>Full compliance with your information security requirements</p></li>
                   <li><i class="bi bi-check-circle-fill"></i><p>Fixed payment for the entire usage period</p></li>
                   <li><i class="bi bi-check-circle-fill"></i><p>Functional improvements according to you</p></li>
                   <li><i class="bi bi-check-circle-fill"></i><p>Strategic safety and independence of your business</p></li>
+                  <li><i class="bi bi-check-circle-fill"></i><p>Customer Profile 360°</p></li>
                 </ul>
+                <p class="num-text">Number of customer profiles</p>
+                <vs-slider disabled="true" @change="cambio" color="#7b98ff" v-model="value3"/>
+                <div style="text-align: center;">
+                  <h3 style="margin: 1.325rem 0 0 0;">Request for price</h3>
+                </div>
               </div>
             </div>
           </div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="container">
+        <div class="home-flexbox">
+          sd
         </div>
       </div>
     </section>
@@ -40,7 +64,23 @@
 
 <script>
 export default {
-  name: "PricingPage"
+  name: "PricingPage",
+  data(){
+    return {
+      value1:5,
+      value2:120,
+      value3: 100,
+      widthx:25,
+      heightx:25
+    }
+  },
+  methods:{
+    cambio(value){
+      this.widthx = value
+      this.heightx = value
+      this.value2 = value * 20
+    }
+  }
 }
 </script>
 
