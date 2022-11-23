66812b5cd8eff8df9c3abee23ab2637eabf5ba25
p0wered <powered.ui@gmail.com>
2022-11-23 19:35:11 +0300

    0.9.5

--- src/pages/PricingPage.vue
+++ src/pages/PricingPage.vue
@@ -95,6 +95,10 @@
         </div>
       </div>
     </section>
+    <footer>
+      <i class="bi bi-c-circle"></i>
+      <p style="margin: 0 0 0 1rem">All Rights Served By MIRAGE {{new Date().getFullYear()}}</p>
+    </footer>
   </div>
 </template>
 
